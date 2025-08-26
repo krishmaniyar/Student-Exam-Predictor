4e0bde458dad470ae892de97630a7f281ce978bb
KBM <160119279+krishmaniyar@users.noreply.github.com>
2025-08-26 18:22:57 +0530

    Data Transformation done

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+import sys
+import numpy as np
+import dill
+
+from src.exception import CustomException
+from src.logger import logging
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
+            logging.info(f"Object saved to {file_path}")
+            logging.info('Read train and test data completed')
+    except Exception as e:
+        raise CustomException(e, sys)
