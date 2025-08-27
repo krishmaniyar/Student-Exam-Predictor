22e41e598c477eddead87b9f740660e6c0da1408
KBM <160119279+krishmaniyar@users.noreply.github.com>
2025-08-27 12:04:50 +0530

    Deployment Configuration

--- src/utils.py
+++ src/utils.py
@@ -1,13 +1,11 @@
 import os
 import sys
-
 import numpy as np 
 import pandas as pd
 import dill
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -15,12 +13,12 @@ def save_object(file_path, obj):
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+            dill.dump(obj, file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
@@ -47,9 +45,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         raise CustomException(e, sys)
     
 def load_object(file_path):
+    print("Model path:", file_path)
+    print("Exists:", os.path.exists(file_path))
+    print("Size:", os.path.getsize(file_path), "bytes")
     try:
         with open(file_path, "rb") as file_obj:
-            return pickle.load(file_obj)
+            return dill.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
